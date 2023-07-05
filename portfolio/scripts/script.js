734c22e64078e5f08e4fe9299ed2fe74c3ef40c8
CalebHaleDev <107201448+CalebHaleDev@users.noreply.github.com>
2023-07-05 15:00:37 -0600

    shrinking navbar created, rough around edges

---
+++ portfolio/scripts/script.js
@@ -0,0 +1,14 @@
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.height = "120px";
+    document.getElementById("logo").style.width = "75px";
+    document.getElementById("navbar-right").getElementsByClassName("nav-link").style.padding = "20px 40px";
+  } else {
+    document.getElementById("navbar").style.height = "160px";
+    document.getElementById("logo").style.width = "100px";
+    document.getElementById("navbar-right").getElementsByClassName("nav-link").style.padding = "40px 40px";
+  }
+}
